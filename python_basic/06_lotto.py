ae55f5e815e5659dedc461983b8d4edf5d556f2b
kimsunwook0108 <0108sunwook@gmail.com>
2022-10-01 15:56:45 +0900

    10/1 lotto

---
+++ python_basic/06_lotto.py
@@ -0,0 +1,13 @@
+import random
+lotto_numbers = []
+lotto_picks = []
+pick = 0
+
+for i in range(45):
+    lotto_numbers.append(i + 1)
+for j in range(7):
+    pick = lotto_numbers.pop(random.randint(0, len(lotto_numbers) - 1))
+    lotto_picks.append(pick)
+
+print(f"예상번호{lotto_picks}")
+print(lotto_numbers)
