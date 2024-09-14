0985a766d1df3a724cb80cec3cf96a837676ab70
kimsunwook0108 <0108sunwook@gmail.com>
2024-09-14 15:50:26 +0900

    09/14 pie차트

---
+++ python_data_A/07_pie.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+# plt.pie([10, 20])
+# plt.show()
+
+# size = [2441, 2312, 1031, 1233]
+# plt.axis('equal')
+# plt.pie(size)
+# plt.show()
+
+# plt.rc('font', family = 'Malgun Gothic')
+# size = [2441, 2312, 1031, 1233]
+# label = ["A형", "B형", "AB형", "O형"]
+# plt.axis('equal')
+# plt.pie(size, labels = label)
+# plt.show()
+
+# plt.rc('font', family = 'Malgun Gotic')
+# size = [2441, 2312, 1031, 1233]
+# label = ["A형", "B형", "AB형", "O형"]
+# plt.axis('equal')
+# plt.pie(size, labels = label, autopct = '%.1f%%')
+# plt.legend()
+# plt.show()
+
+plt.rc('font', family = 'Malgun Gotic')
+size = [2441, 2312, 1031, 1233]
+label = ["A형", "B형", "AB형", "O형"]
+color = ["darkmagenta", "deeppink", "hotpink", "pink"]
+plt.axis('equal')
+plt.pie(size, labels = label, autopct = '%.1f%%', colors = color, explode = [0, 0, 0.2, 0])
+plt.legend()
+plt.show()
